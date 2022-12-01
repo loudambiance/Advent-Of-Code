dcec46c8936c8a438e193733e9eae3e3d152b4c6
Daniel Baucom <Daniel.Baucom@caromonthealth.org>
2022-12-01 13:19:02 -0500

    2020 Day 1

---
+++ 2020/1/c1.py
@@ -0,0 +1,16 @@
+import itertools
+import math
+
+files = ("input_ex.txt", "input.txt")
+
+for file in files:
+    with open(file) as f:
+        lines = f.read().splitlines()
+
+    vals = [eval(i) for i in lines]
+
+    for subset in itertools.combinations(vals, 2):
+        combsum = sum(subset)
+        if combsum == 2020:
+            print(subset)
+            print(math.prod(subset))
