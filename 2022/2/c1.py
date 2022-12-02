df31f1847c3d1d097b66de9060061088e702ac42
Daniel Baucom <Daniel.Baucom@caromonthealth.org>
2022-12-02 10:16:00 -0500

    2022 Day 2

---
+++ 2022/2/c1.py
@@ -0,0 +1,35 @@
+
+
+def main(lines):
+    score = 0
+    for line in lines:
+        player1 = line.split(' ')[0]
+        player2 = line.split(' ')[1]
+        score += getScore(player1, player2)
+    return(score)
+
+
+def getScore(player1, player2):
+    score = 0
+    # Rock = A,X, Paper = B,Y, Scissors = C,Z
+    options = {"A": 1, "B": 2, "C": 3, "X": 1, "Y": 2, "Z": 3}
+    loss = [["A", "Z"], ["B", "X"], ["C", "Y"]]
+    win = [["A", "Y"], ["B", "Z"], ["C", "X"]]
+    player1score = options[player1]
+    player2score = options[player2]
+    if player1score == player2score:
+        score += player2score + 3
+    elif [player1, player2] in loss:
+        score += player2score + 0
+    elif [player1, player2] in win:
+        score += player2score + 6
+    return score
+
+
+if __name__ == "__main__":
+    files = ("input_ex.txt", "input.txt")
+    for file in files:
+
+        with open(file) as f:
+            lines = f.read().splitlines()
+        print(file + ": " + str(main(lines)))
---
+++ 2022/2/c2.py
@@ -0,0 +1,34 @@
+
+
+def main(lines):
+    score = 0
+    for line in lines:
+        player1 = line.split(' ')[0]
+        player2 = line.split(' ')[1]
+        score += getScore(player1, player2)
+    return(score)
+
+
+def getScore(player1, player2):
+    score = 0
+    # Rock = A,M, Paper = B,N, Scissors = C,O
+    options = {"A": 1, "B": 2, "C": 3, "M": 1, "N": 2, "O": 3}
+    loss = {"A":"O", "B": "M", "C": "N"}
+    win = {"A": "N", "B": "O", "C": "M"}
+    tie = {"A": "M", "B": "N", "C": "O"}
+    if player2 == "X":
+        score += options[loss[player1]] + 0
+    elif player2 == "Y":
+        score += options[tie[player1]] + 3
+    elif player2 == "Z":
+        score += options[win[player1]] + 6
+    return score
+
+
+if __name__ == "__main__":
+    files = ("input_ex.txt", "input.txt")
+    for file in files:
+
+        with open(file) as f:
+            lines = f.read().splitlines()
+        print(file + ": " + str(main(lines)))
