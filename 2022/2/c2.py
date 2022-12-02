a8c0161f9549f378b21b8beb3fb230445e95a17a
Daniel Baucom <Daniel.Baucom@caromonthealth.org>
2022-12-02 13:36:33 -0500

    fix spacing

--- 2022/2/c2.py
+++ 2022/2/c2.py
@@ -13,7 +13,7 @@ def getScore(player1, player2):
     score = 0
     # Rock = A,M, Paper = B,N, Scissors = C,O
     options = {"A": 1, "B": 2, "C": 3, "M": 1, "N": 2, "O": 3}
-    loss = {"A":"O", "B": "M", "C": "N"}
+    loss = {"A": "O", "B": "M", "C": "N"}
     win = {"A": "N", "B": "O", "C": "M"}
     tie = {"A": "M", "B": "N", "C": "O"}
     if player2 == "X":
