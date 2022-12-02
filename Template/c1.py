60a11f57eb5d626f05a345ef58790b3dac0a6776
Daniel Baucom <Daniel.Baucom@caromonthealth.org>
2022-12-02 10:17:27 -0500

    turn templates into fuctions

--- Template/c1.py
+++ Template/c1.py
@@ -1,6 +1,10 @@
-files = ("input_ex.txt", "input.txt")
+def main(lines):
+    return(0)
 
-for file in files:
+if __name__ == "__main__":
+    files = ("input_ex.txt", "input.txt")
+    for file in files:
 
-    with open(file) as f:
-        lines = f.read().splitlines()
+        with open(file) as f:
+            lines = f.read().splitlines()
+        print(file + ": " + str(main(lines)))
