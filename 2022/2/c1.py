869e4a2bf1d1d41735224e93a1f86293be8b8d0c
danl <loudambiance@gmail.com>
2022-12-04 19:22:24 -0500

    Add timer

--- 2022/2/c1.py
+++ 2022/2/c1.py
@@ -1,4 +1,4 @@
-
+from time import perf_counter_ns
 
 def main(lines: str):
     score = 0
@@ -29,7 +29,9 @@ def getScore(player1: list, player2: list):
 if __name__ == "__main__":
     files = ("input_ex.txt", "input.txt")
     for file in files:
-
+        t1_start = perf_counter_ns()
         with open(file) as f:
             lines = f.read().splitlines()
         print(file + ": " + str(main(lines)))
+        t1_stop = perf_counter_ns()
+        print(file + " Elapsed time:", t1_stop - t1_start)
--- 2022/3/c1.py
+++ 2022/3/c1.py
@@ -1,3 +1,5 @@
+from time import perf_counter_ns
+
 def main(lines: str):
     runningsum = 0
     for line in lines:
@@ -17,7 +19,9 @@ def main(lines: str):
 if __name__ == "__main__":
     files = ("input_ex.txt", "input.txt")
     for file in files:
-
+        t1_start = perf_counter_ns()
         with open(file) as f:
             lines = f.read().splitlines()
         print(file + ": " + str(main(lines)))
+        t1_stop = perf_counter_ns()
+        print(file + " Elapsed time:", t1_stop - t1_start)
