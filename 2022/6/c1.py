b74d8104eb9c0b9e0f4b22c2997f321f9ca594c6
Daniel Baucom <Daniel.Baucom@caromonthealth.org>
2022-12-06 10:35:46 -0500

    2022 day 6

---
+++ 2022/6/c1.py
@@ -0,0 +1,21 @@
+from time import perf_counter_ns
+
+
+def main(lines: str):
+    for line in lines:
+        for x in range(0, len([*line])-1):
+            if len(set([*line][x:x+4])) == 4:
+                print(x+4)
+                break
+    return(0)
+
+
+if __name__ == "__main__":
+    files = ("input_ex.txt", "input.txt")
+    for file in files:
+        t1_start = perf_counter_ns()
+        with open(file) as f:
+            lines = f.read().splitlines()
+        print(file + ": " + str(main(lines)))
+        t1_stop = perf_counter_ns()
+        print(file + " Elapsed time:", t1_stop - t1_start)
