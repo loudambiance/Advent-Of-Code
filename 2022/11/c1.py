306422173007ccd78dd34fbd38f05d4db1838a33
Daniel Baucom <Daniel.Baucom@caromonthealth.org>
2022-12-12 15:14:16 -0500

    2022 Day 11

---
+++ 2022/11/c1.py
@@ -0,0 +1,57 @@
+from time import perf_counter_ns
+import math
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
+
+def main(lines: list[str]):
+    monkeys = parseData(lines)
+    for x in range(0, 20):
+        for monkey in monkeys.values():
+            while monkey["items"]:
+                old = monkey["items"].pop(0)
+                item = eval(monkey["operation"])
+                item = item//3
+                monkey["runningcount"] += 1
+                if item % monkey["test"] == 0:
+                    monkeys[monkey["true"]]["items"].append(item)
+                else:
+                    monkeys[monkey["false"]]["items"].append(item)
+    values = list()
+    for monkey in monkeys.values():
+        values.append(monkey["runningcount"])
+    values.sort(reverse=True)
+    print(math.prod(values[:2]))
+    return (0)
+
+
+def parseData(lines: list[str]):
+    monkeys = dict()
+    currmonkey = ""
+    for line in lines:
+        if line.startswith("Monkey "):
+            currmonkey = line.lower().removesuffix(":")
+            monkeys[currmonkey] = dict()
+            monkeys[currmonkey]["runningcount"] = 0
+        elif line.startswith("  Starting items:"):
+            monkeys[currmonkey]["items"] = list(map(int, line.replace("  Starting items: ", "").split(", ")))
+        elif line.startswith("  Operation: new = "):
+            monkeys[currmonkey]["operation"] = line.replace("  Operation: new = ", "")
+        elif line.startswith("  Test: divisible by "):
+            monkeys[currmonkey]["test"] = int(line.replace("Test: divisible by ", ""))
+        elif line.startswith("    If true: throw to "):
+            monkeys[currmonkey]["true"] = line.replace("    If true: throw to ", "")
+        elif line.startswith("    If false: throw to "):
+            monkeys[currmonkey]["false"] = line.replace("    If false: throw to ", "")
+    return monkeys
+
+
+if __name__ == "__main__":
+    files = ("input_ex.txt", "input.txt")
+    for file in files:
+        t1_start = perf_counter_ns()
+        with open(file) as f:
+            lines = f.read().splitlines()
+        print(file + ": " + str(main(lines)))
+        t1_stop = perf_counter_ns()
+        print(file + " Elapsed time:", (t1_stop - t1_start)/1000000000)
